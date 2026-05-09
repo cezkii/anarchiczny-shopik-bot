29f995db5554880f2663f5ac46179e9d3aedd5d6
cezkii <cezarymicyk@gmail.com>
2026-05-09 21:41:20 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-```js id="92k21a"
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds]
+intents: [GatewayIntentBits.Guilds]
 });
 
 client.once('ready', () => {
-    console.log(client.user.tag + ' ONLINE');
+console.log(client.user.tag + ' ONLINE');
 });
 
 client.login(process.env.TOKEN);
-```
